747aaa791eabb540cbef8635651eba7b3dd5a1b0
CapKuk <kovaldan@yandex.ru>
2020-07-20 23:39:32 +0700

    Modify.

--- src/my_Main.kt
+++ src/my_Main.kt
@@ -3,15 +3,23 @@ import java.lang.NullPointerException
 
 fun main() {
     print("Введите город, в котором вы живете: ")
-    val city = readLine() ?: "NSK"
+    val city = readLine() ?: getCity()
     print("Какая у вас погода? ")
-    val message = when(readLine()?.toIntOrNull() ?: 0){
+    val message = when(readLine()?.toIntOrNull() ?: getTemp()){
         in -50..15 -> "холодно"
         in 15..25 -> "норм"
         in 25..50 -> "жарко"
         else -> "катастрофа"
     }
     print("В городе $city сейчас $message")
+}
+fun getCity() : String{
+    print("Введена строка с ошибкой ")
+    return  readLine() ?: getCity()
+}
+fun getTemp() : Int{
+    print("Введена строка с ошибкой ")
+    return  readLine()?.toIntOrNull() ?: getTemp()
 }
 
 
