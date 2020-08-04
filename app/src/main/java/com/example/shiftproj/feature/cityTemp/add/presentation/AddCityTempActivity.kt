05155793de1a8e0568733f6d8f4716d072b72522
CapKuk <kovaldan@yandex.ru>
2020-08-05 04:14:10 +0700

    Дописал код для клиента. Возникла ошибка с viewModelScope

--- app/src/main/java/com/example/shiftproj/feature/cityTemp/add/presentation/AddCityTempActivity.kt
+++ app/src/main/java/com/example/shiftproj/feature/cityTemp/add/presentation/AddCityTempActivity.kt
@@ -2,13 +2,14 @@ package com.example.shiftproj.feature.cityTemp.add.presentation
 
 import android.content.Intent
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.add_city_temp_activity.*
 import android.view.Gravity
 import android.widget.Toast
+import androidx.lifecycle.viewModelScope
+import androidx.appcompat.app.AppCompatActivity
 import com.example.shiftproj.R
-import com.example.shiftproj.feature.cityTemp.domain.entity.CityTemp
 import com.example.shiftproj.feature.cityTemp.list.presentation.CitesTempsListActivity
+import kotlinx.android.synthetic.main.add_city_temp_activity.*
+import com.example.common.CityTemp
 
 class AddCityTempActivity : AppCompatActivity(),
     AddCityTempView {
@@ -18,7 +19,13 @@ class AddCityTempActivity : AppCompatActivity(),
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.add_city_temp_activity)
-        presenter.attachView(this)
+        viewModelScope.launch {
+            try {
+                presenter.attachView(this)
+            } catch (e: Exception) {
+                // show error
+            }
+        }
 
         setButton.setOnClickListener {
             presenter.onAddButtonClick(sityTextView.text?.toString())
